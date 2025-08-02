ac3e52c5bf33d55934d998256e7237363907ca81
Nizar Lubbad <nezar.b.lubbbad@gmail.com>
2025-08-02 13:47:28 +0300

    Adds sample pizza data and build my first component
    Introduces a static dataset for pizzas and a new component to display pizza details.
    Lays groundwork for dynamic rendering and improved structure.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,79 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
+
+/**
+ * Two rules for React components:
+ *
+ * 1. Component names must start with a capital letter
+ * 2. Components must return some markup (JSX) or null
+ *
+ */
+
+function Pizza() {
+  // Each component should only return one root element
+  // Self-closing tags must have a closing slash (/) at the end
+  return (
+    <div>
+      <img src="pizzas/spinaci.jpg" alt="Pizza Spinaci" />
+      <h1>Pizza Spinaci</h1>
+      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
+    </div>
+  );
+}
+
 function App() {
-  return <h1>Hello, World!</h1>;
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <Pizza />
+      <Pizza />
+    </div>
+  );
 }
 
 // React 18+ root API
@@ -14,5 +85,5 @@ root.render(
   </React.StrictMode>
 );
 
-// React v17
-// React.render(<App />); // and the import is different
+// React before v18
+// ReactDOM.render(<App />, document.getElementById("root")); // and the import is different
